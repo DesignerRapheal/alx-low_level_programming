9c6dbd81d8846976f656254c44ccdcd9e936bd42
DesignerRapheal <owoyemidamilola2015@gmail.com>
2023-04-05 00:58:21 -0700

    singly linked

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,23 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_integer_list - prints all the elements of an integer list
- * @head: head of the list
+ * print_list - Prints all the elements of a list.
+ * @list_head: A pointer to the head of the list.
  *
- * Return: number of elements in the list
+ * Return: The number of nodes in the list.
  */
-size_t print_integer_list(const int_list_t *head)
+size_t print_list(const list_t *list_head)
 {
 	size_t num_nodes = 0;
-	const int_list_t *current_node = head;
+	const list_t *current_node = list_head;
 
 	while (current_node != NULL)
 	{
-		printf("%d\n", current_node->value);
+		printf("%d\n", current_node->n);
 		current_node = current_node->next;
 		num_nodes++;
 	}
 
 	return (num_nodes);
 }
-
